6e70d08a15ca6991dc2b8962cb473fed0a6cd45b
Illiaz <illia.zhurbenko@gmail.com>
2025-10-03 05:40:13 +0300

    .

--- src/main.cpp
+++ src/main.cpp
@@ -4,38 +4,45 @@
 #include <Entity.h>
 #include <UUID.h>
 #include <Scene.h>
-#include <glm/glm.hpp>
-#include <glm/vec3.hpp>
-
 
 int main() {
 
     Engine::Initialize();
 
-	/*SceneSerializer ss; // загружает сцену из файла, но ее пока шо нет
+    /*SceneSerializer ss; // загружает сцену из файла, но ее пока шо нет
      ss.Deserialize("scene.yaml");*/
 
     Engine::GetRender().init_buffers(Window::GetWindowWidth(), Window::GetWindowHeight());
 
     Engine::InputInitialize();
 
-	//все что выше не трогать, это инициализация движка
+    //все что выше не трогать, это инициализация движка
+
 
     Entity earth("Earth", "assets/objects/SpaceItems/Earth_1_12756.glb");
     earth.set_scale(glm::vec3(0.3F));
+    earth.set_position(glm::vec3(0.0f, -10.0f, 0.0f));
     Scene::createEntity(earth);
 
-	//где то тут можешь делать все что угодно, например создавать энтити и добавлять их в сцену
-    
+    //где то тут можешь делать все что угодно, например создавать энтити и добавлять их в сцену
+
+
+
     while (!Window::ShouldClose())
     {
+
+        // Render::getLightSources().set_directional_light(0, glm::normalize(-earth_pos), glm::vec3{ 6.0f });
+        // берем сущевствующий свет и меняем его направленность
+
+       // Render::getLightSources().send_changes(); отправляем все изменения с освещением в шейдеры
+
         // до тик инпута
         Engine::Tick(); //тут небольшой просак, потом разделю тик
         // после прохода отрисовки
 
-        
 
-		Window::SwapBuffers();//конец кадра
+
+        Window::SwapBuffers();//конец кадра
     }
     Engine::Clear();
 
